cfc67e85f4dedff3ff773f54c29004c3e51aae03
y4z <dev@y4z.dev>
2026-03-05 19:30:54 +0000

    added: basic tcp handshake server and client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod tcp_client;
+mod tcp_server;
+
+use std::io::Result;
+use std::thread::{self, JoinHandle};
+
+fn main() -> Result<()> {
+    // Spawn server in a separate thread
+    let server_handle: JoinHandle<()> = thread::spawn(|| match tcp_server::server_main() {
+        Ok(_) => println!("server finished"),
+        Err(e) => println!("server error: {}", e),
+    });
+
+    // Run client on main thread
+    match tcp_client::client_main() {
+        Ok(_) => println!("Client finished success"),
+        Err(e) => eprintln!("Client error: {}", e),
+    }
+
+    // Wait for server thread to finish
+    server_handle.join().expect("could not join server thread");
+
+    println!("done all !");
+    Ok(())
 }
